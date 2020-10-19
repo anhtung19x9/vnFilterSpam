57ab69391bbaf95dd39a28e152b30b772ae5066b
Tung Trinh <anhtung19x9@gmail.com>
2020-10-19 13:55:42 +0700

    Update Window GUI and New Main function to run windows

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+from mainWindows import Ui_Dialog
+from PyQt5 import QtWidgets, uic
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class MainWindows(QtWidgets.QMainWindow, Ui_Dialog):
+    def __init__(self,  *args, obj=None, **kwargs):
+        super(MainWindows, self).__init__(*args,*kwargs)
+        self.setupUi(self)
+
+app = QtWidgets.QApplication(sys.argv)
+windows = MainWindows()
+windows.show()
+app.exec_()
+
+if __name__ == '__main__':
+    print('PyCharm')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-#if __name__ == '__main__':
-#    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
